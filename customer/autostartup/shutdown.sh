0cc06a9eaf43753ced1148265448ec092038eb1d
Austin Chambers <icarusdignata@gmail.com>
2020-04-21 09:32:05 -0700

    Reliability improvement to startup/shutdown script

--- customer/autostartup/shutdown.sh
+++ customer/autostartup/shutdown.sh
@@ -7,11 +7,10 @@ kubectl config use-context $KUBE_CONTEXT
 
 echo "Stopping Replicated"
 REPLICATED_POD_ID=$(kubectl get pod -l "app=replicated,tier=master" -o name | sed 's/pod\///')
-kubectl exec $REPLICATED_POD_ID -c replicated -- replicatedctl app stop --force --attach
+kubectl exec $REPLICATED_POD_ID -c replicated -- replicatedctl app stop --force --attach || true
 
-# Prevents issues on shutdown due to fs-snapshotter deployment
 echo 'Delete replicated-shared-fs-snapshotter deployment'
-kubectl delete deployments/replicated-shared-fs-snapshotter || true 
+kubectl delete deployments/replicated-shared-fs-snapshotter || true
 
 echo "Stopping Ceph"
 kubectl -n rook-ceph-system scale deploy rook-ceph-operator --replicas=0
@@ -21,8 +20,12 @@ echo "Cordoning Nodes"
 kubectl cordon $I1 & kubectl cordon $I2 & kubectl cordon $I3 & kubectl cordon $I4 & kubectl cordon $I5 & kubectl cordon $I6 & wait
 
 echo "Node Shutdown"
-# Equivalent AWS command (for each node + LB): aws ec2 stop-instances --instance-ids i-07c817fd03dba9e0f && aws ec2 wait instance-stopped --instance-ids i-07c817fd03dba9e0f
-gcloud compute instances stop $I1 --zone $I1Z & gcloud compute instances stop $I2 --zone $I2Z & gcloud compute instances stop $I3 --zone $I3Z & gcloud compute instances stop $I4 --zone $I4Z & gcloud compute instances stop $I5 --zone $I5Z & gcloud compute instances stop $I6 --zone $I6Z & wait
+# Equivalent AWS commands (for each node + LB): 
+# * aws ec2 stop-instances --instance-ids i-07c817fd03dba9e0f
+# * aws ec2 wait instance-stopped --instance-ids i-07c817fd03dba9e0f
+gcloud compute instances stop $I1 --zone $I1Z & gcloud compute instances stop $I2 --zone $I2Z \
+  & gcloud compute instances stop $I3 --zone $I3Z & gcloud compute instances stop $I4 --zone $I4Z \
+  & gcloud compute instances stop $I5 --zone $I5Z & gcloud compute instances stop $I6 --zone $I6Z & wait
 gcloud compute instances stop $LB --zone $LBZ & wait
 
 echo "Successful Shutdown"
