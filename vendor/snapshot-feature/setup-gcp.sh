dded0ba2f86f0256bae31747cee154e9de0670ad
Austin Chambers <icarusdignata@gmail.com>
2020-05-20 17:37:34 -0700

    Improvements to GCP setup script for snapshots

--- vendor/snapshot-feature/setup-gcp.sh
+++ vendor/snapshot-feature/setup-gcp.sh
@@ -2,24 +2,57 @@
 
 ## Script to help set up GCP storage for snapshot functionality
 ## Requires: gsutil and for your account project to be linked via command line. 
-## Validated on OSX shell
+## Validated on OSX shell following the instructions here: https://github.com/vmware-tanzu/velero-plugin-for-gcp#setup
 
-BUCKET_NAME=replsnap-$(LC_CTYPE=C tr -dc a-z0-9 < /dev/urandom | head -c 16 | xargs)
+## Get necessary gcloud settings
+PROJECT_ID=$(gcloud config get-value project)
+
 ## Create the bucket
-gsutil mb -l US gs://$BUCKET_NAME
-## Create the iam account
-gcloud iam service-accounts create "$BUCKET_NAME" --display-name="$BUCKET_NAME"
-## Create the key linked to the IAM account and save
-PROJECT_NAME=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
-IAM_ACCT="${BUCKET_NAME}@${PROJECT_NAME}.iam.gserviceaccount.com"
-gcloud iam service-accounts keys create "./${BUCKET_NAME}.key" --iam-account="$IAM_ACCT"
+BUCKET=replsnap$(LC_CTYPE=C tr -dc a-z0-9 < /dev/urandom | head -c 16 | xargs)
+gsutil mb -l US gs://$BUCKET
 
-## Add role to service account. 
-gcloud projects add-iam-policy-binding $PROJECT_NAME \
-  --member serviceAccount:$IAM_ACCT \
-  --role "roles/storage.admin"
+## Create the iam service account
+gcloud iam service-accounts create "$BUCKET" --display-name="Velero $BUCKET"
 
-## Copy key to Replicated Admin Console: /app/airgapped/snapshots/settings -- hycKNpgmi
-pbcopy < "./${BUCKET_NAME}.key" 
-printf "\n\nBUCKET_NAME: $BUCKET_NAME\n\n"
-echo "Enter bucket name, path, and paste key to Replicated Admin Console /app/airgapped/snapshots/settings"
+## Set the $SERVICE_ACCOUNT_EMAIL variable to match its email value.
+SERVICE_ACCOUNT_EMAIL=$(gcloud iam service-accounts list \
+  --filter="displayName:Velero $BUCKET" \
+  --format 'value(email)')
+
+## Attach policies to give velero the necessary permissions to function
+ROLE_PERMISSIONS=(
+    compute.disks.get
+    compute.disks.create
+    compute.disks.createSnapshot
+    compute.snapshots.get
+    compute.snapshots.create
+    compute.snapshots.useReadOnly
+    compute.snapshots.delete
+    compute.zones.get
+)
+
+gcloud iam roles create velero.server.$BUCKET \
+    --project $PROJECT_ID \
+    --title "Velero Server $BUCKET" \
+    --permissions "$(IFS=","; echo "${ROLE_PERMISSIONS[*]}")"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member serviceAccount:$SERVICE_ACCOUNT_EMAIL \
+    --role projects/$PROJECT_ID/roles/velero.server.$BUCKET
+
+gsutil iam ch serviceAccount:$SERVICE_ACCOUNT_EMAIL:objectAdmin gs://${BUCKET}
+
+## Create a service account key, specifying an output file (credentials-velero) in your local directory:
+gcloud iam service-accounts keys create credentials-velero.$BUCKET \
+    --iam-account $SERVICE_ACCOUNT_EMAIL
+
+printf "\n\nBUCKET: $BUCKET\n"
+printf "Key File: credentials-velero.$BUCKET\n\n"
+
+## Install Velero
+velero install \
+    --provider gcp \
+    --plugins velero/velero-plugin-for-gcp:v1.0.1 \
+    --bucket $BUCKET \
+    --secret-file ./credentials-velero.$BUCKET \
+    --use-restic
